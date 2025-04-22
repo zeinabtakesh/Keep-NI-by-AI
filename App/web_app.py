df2619635bd024337f28d1006371efc1d81979a5
FatimaaIsmail27 <139501201+FatimaaIsmail27@users.noreply.github.com>
2025-04-22 14:42:07 +0300

    web_app.py

--- App/web_app.py
+++ App/web_app.py
@@ -182,4 +182,5 @@ def start_monitoring():
 
 # ----------------- Start App -----------------
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
