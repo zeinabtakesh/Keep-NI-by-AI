5ec944f9c8d98c98d58f3cf78f7aef85872f631f
FatimaaIsmail27 <139501201+FatimaaIsmail27@users.noreply.github.com>
2025-04-23 13:32:29 +0300

    Update web_app.py

--- App/web_app.py
+++ App/web_app.py
@@ -237,4 +237,4 @@ def start_monitoring():
 
 # ----------------- Start App -----------------
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)  # ← Add host and port
