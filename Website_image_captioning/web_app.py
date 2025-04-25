5f349d97b37007400e508029b4d113d9d39c10b2
zeinabtakesh <zrt03@mail.aub.edu>
2025-04-26 01:20:52 +0300

    Update web_app.py

--- Website_image_captioning/web_app.py
+++ Website_image_captioning/web_app.py
@@ -303,5 +303,4 @@ def clear_alerts_json():
 
 # ----------------- Start App -----------------
 if __name__ == '__main__':
-if __name__ == '__main__':
         app.run(host='0.0.0.0', port=5000, debug=True)
